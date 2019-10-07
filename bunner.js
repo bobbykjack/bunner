76f12940075838f09e402f3bf3719892e582b0c4
Bobby Jack <bobbyjack@gmail.com>
2019-10-07 10:12:37 +0100

    refactoring; data in json file; better canvas layout

--- bunner.js
+++ bunner.js
@@ -1,80 +1,73 @@
 
-'use strict';
+"use strict";
 
-// Bunner, version 0.3
+// Bunner, version 0.4
 
-// This version will include both the first two levels, plus a new one. It will
-// be the first time we've had multiple levels, so it's a good opportunity to
-// stop hardcoding things like win conditions.
+// Lots of refactoring to make everything a lot cleaner. Data (levels and
+// sprites) now stored in json file and loaded via ajax.
 
-// Also proper movement
-
-const version = 0.3,
-    levels = [
-        [ "g", "b" ],
-        [ "gg", "bf" ],
-// The level now consists of 2 tiles
-        [ "gg\ngg", "b \n f" ]
-    ],
+const version = 0.4,
     tileSize = 32,
     canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d"),
+    ctx = canvas.getContext("2d");
 
-// A new canvas to show messages to the user
-    dialogCanvas = document.getElementById("dialog"),
-    dialogCtx = dialogCanvas.getContext("2d");
+const SCREEN_TITLE = 0,
+    SCREEN_LEVEL = 1,
+    SCREEN_WAITING = 2,
+    SCREEN_FINISH = 3;
 
-var level = 0,
+var w = 800,
+    h = 600,
+    levels = [],
+    level = 0,
     terrain,
     objects,
-    screenNum,
+// This replaces screenNum, which was confusing.
+    screenType,
     height,
     width,
-
-    movedOnto,
-
-// This was a major omission from version 0.1! Keep track of how many turns
-// we've taken.
     turn,
+    gutter = 1,
+    flags = {
+        drawGrid: 1
+    },
+// Object sprites are now stored in a json file
+    objectSprites = {};
 
-// The bunner sprite. It's chunky, but it'll do for a first go.
-    bunner = `
-   bb
-  bdbb
- bdlbe
- bdlbb
-bbdlbbb
-bbbdbb
- bbbbb
-b bbbbb`,
-
-// A sprite for the 'flag' which will represent our winning tile in this level
-    flag = `
-  xxgxx
-  xgggx
-  xxgxx
-  xxxxx
-     xd
-     xd
-     xd
-     xd`,
-  objectSprites = {};
-
-init();
-
+loadData();
 
 /**
- * Split up a string into a two-dimensional array representing values in the
- * grid
+ * Had some 'blurry line' issues. Not convinced this was the actual solution,
+ * but seems like good idea for now.
+ *
+ * @see https://stackoverflow.com/a/56997819/5058
  */
-function processDataString(str, trim) {
-    if (trim) {
-        str = str.trim();
-    }
+function getPixelRatio(context) {
+    var dpr = window.devicePixelRatio || 1,
+        bsr = context.webkitBackingStorePixelRatio ||
+            context.mozBackingStorePixelRatio ||
+            context.msBackingStorePixelRatio ||
+            context.oBackingStorePixelRatio ||
+            context.backingStorePixelRatio || 1;
 
-    str = str.split("\n"),
-    str.forEach(function(v, i, a) { a[i] = a[i].split(""); });
-    return str;
+  return dpr / bsr;
+}
+
+/**
+ * Use ajax to load level and sprite data from json file.
+ */
+function loadData() {
+    var request = new XMLHttpRequest();
+    request.open("get", "data.json");
+
+    request.addEventListener("load", function() {
+        var data = JSON.parse(this.responseText);
+        levels = data.levels;
+        objectSprites = data.sprites;
+        init();
+    });
+
+    request.send();
 }
 
 /**
@@ -82,52 +75,61 @@ function processDataString(str, trim) {
  * input, and show the title screen.
  */
 function init() {
-    processObjectSprites();
+// Adjust canvas to try to cater for retina screens
+    var ratio = getPixelRatio(ctx);
+    canvas.width = w * ratio;
+    canvas.height = h * ratio;
+    canvas.style.width = w + "px";
+    canvas.style.height = h + "px";
+    ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
 
 // All events are now caught via the keyup listener ...
-    window.addEventListener('keyup', function(ev) {
-        if (screenNum == 0) {
+    window.addEventListener("keyup", function(ev) {
+        if (screenType == SCREEN_TITLE) {
             showLevel();
-        } else if (screenNum == 1) {
+        } else if (screenType == SCREEN_LEVEL) {
             var moved = false,
-                redraw = false;
+                skipped = false;
 
             if (ev.keyCode == 32) {
-                moved = true;
+                skipped = true;
             } else if (ev.keyCode == 37 || ev.keyCode == 65) {
                 moved = moveBunner(-1, 0);
-                redraw = true;
             } else if (ev.keyCode == 38 || ev.keyCode == 87) {
                 moved = moveBunner(0, -1);
-                redraw = true;
             } else if (ev.keyCode == 39 || ev.keyCode == 68) {
                 moved = moveBunner(1, 0);
-                redraw = true;
             } else if (ev.keyCode == 40 || ev.keyCode == 83) {
                 moved = moveBunner(0, 1);
-                redraw = true;
+// "g" to toggle gridlines
+            } else if (ev.keyCode == 71) {
+                flags.drawGrid = !flags.drawGrid;
+                drawLevel();
+// "m" to increase 'margin' — the space between tiles
+            } else if (ev.keyCode == 77) {
+                if (++gutter > 4) {
+                    gutter = 0;
+                }
+
+                drawLevel();
             }
 
-            if (moved) {
+            if (moved || skipped) {
                 turn++;
 
-                if (redraw) {
+                if (moved) {
                     drawLevel();
                 }
 
                 endTurn();
             }
-
-
-        } else if (screenNum == 2) {
-            clearDialog();
-
+        } else if (screenType == SCREEN_WAITING) {
             if (++level > levels.length - 1) {
                 win();
             } else {
                 showLevel();
             }
-        } else if (screenNum == 3) {
+        } else if (screenType == SCREEN_FINISH) {
             titleScreen();
         }
     });
@@ -136,60 +138,84 @@ function init() {
 }
 
 /**
- * Process the game state at the end of a turn. Currently, win conditions for
- * each level are hard-coded here.
+ * Process the game state at the end of a turn.
  */
 function endTurn() {
-// We win the first level just by having made a move, and we win the other two
-// levels if we just moved onto the flag.
-    if ((level == 0) || ((level == 1 || level == 2) && movedOnto == "f")) {
+    var cond = levels[level].win,
+        win = false,
+        obj,
+        bunner = getBunner(),
+        i;
+
+// [...]
+    if (cond.hasOwnProperty("turn") && cond.turn == turn) {
+        win = true;
+    } else if (cond.hasOwnProperty("standing") && bunner) {
+        for (i = 0; i < levels[level].objects.length; i++) {
+            obj = levels[level].objects[i];
+
+            if (obj.type == cond.standing && objectsTogether(bunner, obj)) {
+                win = true;
+            }
+        }
+    }
+
+    if (win) {
         dialog(winMessage());
-        screenNum = 2;
+        screenType = SCREEN_WAITING;
     }
 }
 
 /**
- * Move Bunner x tiles horizontally and y tiles vertically
+ * Determine if two objects share the same tile.
+ */
+function objectsTogether(obj1, obj2) {
+    return obj1.x == obj2.x && obj1.y == obj2.y;
+}
+
+/**
+ * Find the bunner object.
+ */
+function getBunner() {
+    var i;
+
+    for (i = 0; i < levels[level].objects.length; i++) {
+        if (levels[level].objects[i].type == "b") {
+            return levels[level].objects[i];
+        }
+    }
+}
+
+/**
+ * Try to move the bunner object x tiles horizontal and y tiles vertical.
  */
 function moveBunner(x, y) {
-    var tx, ty, newx, newy, inbounds
+    var bunner;
 
-    for (ty = 0; ty < objects.length; ty++) {
-        for (tx = 0; tx < objects[ty].length; tx++) {
-            if (objects[ty][tx] == "b") {
-                newx = tx + x;
-                newy = ty + y;
-
-                inbounds = newx >= 0 && newx < objects[ty].length && newy >= 0
-                    && newy < objects.length;
-
-                if (inbounds) {
-                    movedOnto = objects[newy][newx];
-                    objects[ty][tx] = 0;
-                    objects[newy][newx] = "b";
-                    return true;
-                }
-            }
-        }
+    if (bunner = getBunner()) {
+        return moveObjectTo(bunner, bunner.x + x, bunner.y + y);
     }
 
     return false;
 }
 
 /**
- * Convert sprite data into a slightly more-easy-to-work-with format
+ * Move an object to a given tile
  */
-function processObjectSprites() {
-// This is a bit messy; we're just hard-coding a couple of sprites right now
-    objectSprites["b"] = processDataString(bunner);
-    objectSprites["b"].shift();
+function moveObjectTo(obj, x, y) {
+    var lev = levels[level];
 
-    objectSprites["f"] = processDataString(flag);
-    objectSprites["f"].shift();
+    if (x >= 0 && x < lev.width && y >= 0 && y < lev.height) {
+        obj.x = x;
+        obj.y = y;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
- * Get the 'win' message, including a not about how many turns the player took
+ * Get the 'win' message, including a note about how many turns the player took.
  */
 function winMessage() {
     return "Well done, you beat this level in " + turn + " turn"
@@ -197,20 +223,38 @@ function winMessage() {
 }
 
 /**
- * Populate the dialog canvas with a message 
+ * Get a message representing the current win condition
  */
-function dialog(msg) {
-    dialogCtx.font = "16px sans-serif";
-    dialogCtx.fillStyle = "rgb(0, 0, 0)";
-    dialogCtx.fillText(msg, 0, 20);
+function winConditionMessage() {
+    var typeName = {
+        "b": "bunner",
+        "f": "flag"
+    };
+
+    if (levels[level].win.hasOwnProperty("turn")) {
+        return "win: by completing " + levels[level].win.turn + " turn"
+            + (levels[level].win.turn == 1 ? "" : "s");
+    } else if (levels[level].win.hasOwnProperty("standing")) {
+        return "win by stepping on a " + typeName[levels[level].win.standing];
+    } else {
+        return "win condition unknown!";
+    }
 }
 
 /**
- * Remove all text from the dialog by painting over the whole thing
+ * Populate the dialog area with a message 
  */
-function clearDialog() {
-    dialogCtx.fillStyle = "rgb(255, 255, 255)";
-    dialogCtx.fillRect(0, 0, dialogCanvas.width, dialogCanvas.height);
+function dialog(msg) {
+    ctx.fillStyle = "rgba(255, 255, 255, 0.9)";
+    ctx.fillRect(10, 10, w - 20, 32);
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.strokeRect(10.5, 10.5, w - 20, 32);
+
+    ctx.font = "16px sans-serif";
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillText(msg, w / 2, 32);
 }
 
 /**
@@ -219,10 +263,10 @@ function clearDialog() {
  */
 function titleScreen() {
     var x;
-    screenNum = 0;
+    screenType = SCREEN_TITLE;
     level = 0;
     infoScreen();
-    x = canvas.width / 2;
+    x = w / 2;
     ctx.fillText("Bunner — a game by bobbyjack", x, 40);
     ctx.fillText("Version " + version, x, 80);
     ctx.fillText("Cursor keys to move, Space to skip turn", x, 120);
@@ -234,14 +278,10 @@ function titleScreen() {
  * whenever we start the level - maybe needs a better name!
  */
 function showLevel() {
-    screenNum = 1;
+    screenType = SCREEN_LEVEL;
     turn = 0;
-
-    terrain = processDataString(levels[level][0], true);
-    objects = processDataString(levels[level][1], true);
-
-    height = terrain.length;
-    width = terrain[0].length;
+    height = levels[level].height;
+    width = levels[level].width;
 
     if (height < 1 || width < 1) {
 // Should probably, one day, report this error via the canvas, although this
@@ -251,16 +291,21 @@ function showLevel() {
         return;
     }
 
-    canvas.width = tileSize * width;
-    canvas.height = tileSize * height;
-
     drawLevel();
+    dialog(winConditionMessage());
 }
 
 /**
  * Draw the current level by drawing, first, terrain, then objects on top.
  */
 function drawLevel() {
+    ctx.fillStyle = "rgb(255, 255, 255)";
+    ctx.fillRect(0, 0, w, h);
+
+    if (flags.drawGrid) {
+        drawGrid();
+    }
+
     drawTerrain();
     drawObjects();
 }
@@ -272,15 +317,68 @@ function drawTerrain() {
     var y = 0,
         x,
         colors = {
-            "g": [ 0, 200, 0 ]
+            "g": [ 0, 200, 0 ],
+            "r": [ 200, 0, 0 ]
         };
 
-    for (y = 0; y < terrain.length; y++) {
-        for (x = 0; x < terrain[y].length; x++) {
-            if (setColor(ctx, colors, terrain[y][x])) {
-                ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+// Lots of calculation to work out our exact pixels to draw at. Takes into
+// account: width & height of level playfield, width & height of canvas, tile
+// size, and gutter.
+    var width = levels[level].width,
+        height = levels[level].height,
+        tilesH = Math.floor((w - gutter) / (tileSize + gutter)),
+        tilesV = Math.floor((h - gutter) / (tileSize + gutter)),
+        tileX = Math.floor((tilesH / 2) - (width / 2)),
+        tileY = Math.floor((tilesV / 2) - (height / 2)),
+        baseX = Math.floor((w - (tilesH * (tileSize + gutter)) - gutter ) / 2),
+        baseY = Math.floor((h - (tilesV * (tileSize + gutter)) - gutter ) / 2);
+
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            if (setColor(ctx, colors, levels[level].terrain)) {
+                drawTerrainTile(
+                    baseX + ((tileX + x) * (tileSize + gutter)) + gutter,
+                    baseY + ((tileY + y) * (tileSize + gutter)) + gutter,
+                    levels[level].terrain
+                );
             }
         }
+    }
+}
+
+/**
+ * Draw a terrain tile. In due course, this will mirror what we do for objects —
+ * i.e. we'll have actual terrain sprites, rather than just colored tiles. Can
+ * also add complexity here to, for example, give several different 'grass'
+ * tiles so they don't just appear uniform.
+ */
+function drawTerrainTile(x, y, type) {
+    ctx.fillRect(x, y, tileSize, tileSize);
+}
+
+/**
+ * Draw gridlines. This is just really for development / testing, unless I
+ * decide I actually *like* tha aesthetic of a grid.
+ */
+function drawGrid() {
+    ctx.fillStyle = "rgb(100, 100, 100)";
+
+    var tilesH = Math.floor((w - gutter) / (tileSize + gutter)),
+        tilesV = Math.floor((h - gutter) / (tileSize + gutter)),
+        baseX = Math.floor((w - (tilesH * (tileSize + gutter)) - gutter ) / 2),
+        baseY = Math.floor((h - (tilesV * (tileSize + gutter)) - gutter ) / 2),
+        i;
+
+    ctx.fillRect(baseX, 0, gutter, h);
+
+    for (i = 0; i < tilesH; i++) {
+        ctx.fillRect(baseX + (i + 1) * (tileSize + gutter), 0, gutter, h);
+    }
+
+    ctx.fillRect(0, baseY, w, gutter);
+
+    for (i = 0; i < tilesV; i++) {
+        ctx.fillRect(0, baseY + (i + 1) * (tileSize + gutter), w, gutter);
     }
 }
 
@@ -288,15 +386,35 @@ function drawTerrain() {
  * Draw all the objects onto the canvas
  */
 function drawObjects() {
-    var y = 0,
-        x;
+// Note that all this is repeated from drawTerrain; would be nice to factor out
+    var width = levels[level].width,
+        height = levels[level].height,
+        tilesH = Math.floor((w - gutter) / (tileSize + gutter)),
+        tilesV = Math.floor((h - gutter) / (tileSize + gutter)),
+        tileX = Math.floor((tilesH / 2) - (width / 2)),
+        tileY = Math.floor((tilesV / 2) - (height / 2)),
+        baseX = Math.floor((w - (tilesH * (tileSize + gutter)) - gutter ) / 2),
+        baseY = Math.floor((h - (tilesV * (tileSize + gutter)) - gutter ) / 2),
+        x,
+        y,
+        i,
+        obj,
+        seen = [];
 
-    for (y = 0; y < objects.length; y++) {
-        for (x = 0; x < objects[y].length; x++) {
-            if (objectSprites.hasOwnProperty(objects[y][x])) {
-                drawObject(x * tileSize, y * tileSize, objects[y][x]);
-            }
+    for (i = 0; i < levels[level].objects.length; i++) {
+        obj = levels[level].objects[i];
+
+// Now that multiple objects can exist within the same tile, there's an issue
+// of how to draw them. For now, just draw *the first* object, as defined in the
+// level data.
+        if (seen.indexOf(obj.x + "," + obj.y) !== -1) {
+            continue;
         }
+
+        seen.push(obj.x + "," + obj.y);
+        x = baseX + ((tileX + obj.x) * (tileSize + gutter)) + gutter;
+        y = baseY + ((tileY + obj.y) * (tileSize + gutter)) + gutter;
+        drawObject(x, y, obj.type);
     }
 }
 
@@ -306,23 +424,11 @@ function drawObjects() {
 function drawObject(x, y, id) {
     var x2,
         y2,
-        objectColors = {
-            "b": {
-                "b": [ 57, 36, 36 ],
-                "d": [ 23, 0, 0 ],
-                "l": [ 220, 207, 174 ],
-                "e": [ 226, 180, 40 ]
-            },
-            "f": {
-                "x": [ 70, 70, 70 ],
-                "g": [ 255, 0, 0 ],
-                "d": [ 0, 0, 0 ]
-            }
-        };
+        sprite = objectSprites[id];
 
-    for (y2 = 0; y2 < objectSprites[id].length; y2++) {
-        for (x2 = 0; x2 < objectSprites[id][y2].length; x2++) {
-            if (setColor(ctx, objectColors[id], objectSprites[id][y2][x2])) {
+    for (y2 = 0; y2 < sprite.tiles.length; y2++) {
+        for (x2 = 0; x2 < sprite.tiles[y2].length; x2++) {
+            if (setColor(ctx, sprite.palette, sprite.tiles[y2][x2], 0.9)) {
                 ctx.fillRect(x + (x2 * 4), y + (y2 * 4), 4, 4);
             }
         }
@@ -333,19 +439,24 @@ function drawObject(x, y, id) {
  * Set the fill style on the given context, according to the value in the colors
  * map, indexed by the key
  */
-function setColor(ctx, colors, key) {
+function setColor(ctx, colors, key, opacity) {
     var color;
+    opacity = opacity || 1;
 
     if (colors.hasOwnProperty(key)) {
         color = colors[key];
-
-        ctx.fillStyle =
-            "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")";
-
+        ctx.fillStyle = rgba(color[0], color[1], color[2], opacity);
         return true;
     }
 
     return false;
+}
+
+/**
+ * Helper function for building rgba() strings
+ */
+function rgba(r, g, b, a) {
+    return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
 }
 
 /**
@@ -354,9 +465,9 @@ function setColor(ctx, colors, key) {
  */
 function win() {
     var x;
-    screenNum = 3;
+    screenType = SCREEN_FINISH;
     infoScreen();
-    x = canvas.width / 2;
+    x = w / 2;
     ctx.fillText("CONGRATULATIONS!", x, 40);
     ctx.fillText("You won by beating all " + levels.length + " levels.", x, 80);
     ctx.fillText("Press any key to return to title screen", x, 160);
@@ -367,10 +478,8 @@ function win() {
  * screens.
  */
 function infoScreen() {
-    canvas.width = 500
-    canvas.height = 200;
     ctx.fillStyle = "rgb(255, 255, 255)";
-    ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+    ctx.fillRect(0, 0, w, h);
     ctx.font = "20px sans-serif";
     ctx.textAlign = "center";
     ctx.fillStyle = "rgb(0, 0, 0)";
